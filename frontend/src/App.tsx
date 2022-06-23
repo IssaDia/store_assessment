c5ef86596628e24bfab679f6acc3fcbe277caa0d
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-06-23 14:24:07 +0800

    dummy products displayed on front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,33 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import { products } from "../src/lib/data/Products";
+import { ProductInterface } from "./lib/interfaces/ProductInterface";
+import Products from "./components/Products/Products";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [productList, setProductList] = useState<ProductInterface[]>(products);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="grid-container">
+      <Header />
+      <h1>ProductList</h1>
+      <div className="content flex flex-row">
+        <div className="main w-3/4">
+          <div className="products grid grid-cols-3 gap-x-8 gap-y-4">
+            {products.map((product, index: number) => {
+              return (
+                <div>
+                  <p key={index}>{product.name}</p>
+                  <button>Add to order</button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="sidebar w-1/4">Cart Item</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/lib/data/Products.ts
@@ -0,0 +1,25 @@
+import { ProductInterface } from "../interfaces/ProductInterface";
+
+export const products: ProductInterface[] = [
+  {
+    code: 1,
+    name: "product1",
+    quantity: 100,
+  },
+  {
+    code: 2,
+    name: "product2",
+    quantity: 500,
+  },
+  {
+    code: 3,
+    name: "product3",
+    quantity: 1000,
+  },
+  {
+    code: 3,
+    name: "product3",
+    quantity: 1000,
+  },
+];
+
