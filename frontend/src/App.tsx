290a0f8d8a7087dfedea9140d0f037301a00b94d
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-06-23 15:33:37 +0800

    cart is functionnal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,28 @@ import Header from "./components/Header";
 import { products } from "../src/lib/data/Products";
 import { ProductInterface } from "./lib/interfaces/ProductInterface";
 import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   const [productList, setProductList] = useState<ProductInterface[]>(products);
+  const [orderItems, setOrderItems] = useState<ProductInterface[]>([]);
+
+  function addToCart(product: ProductInterface) {
+    let alreadyInCart = false;
+    [...orderItems].forEach((item) => {
+      if (item.code === product.code) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      setOrderItems([...orderItems, product]);
+    }
+  }
+
+  function removeFromCart(product: ProductInterface) {
+    setOrderItems([...orderItems].filter((item) => item.code !== product.code));
+  }
 
   return (
     <div className="grid-container">
@@ -16,15 +35,37 @@ function App() {
           <div className="products grid grid-cols-3 gap-x-8 gap-y-4">
             {products.map((product, index: number) => {
               return (
-                <div>
-                  <p key={index}>{product.name}</p>
-                  <button>Add to order</button>
+                <div key={index}>
+                  <p>{product.name}</p>
+                  <button onClick={() => addToCart(product)}>
+                    Add to order
+                  </button>
                 </div>
               );
             })}
           </div>
         </div>
-        <div className="sidebar w-1/4">Cart Item</div>
+        <div className="sidebar w-1/4">
+          {orderItems.length === 0 ? (
+            <div>Cart is empty</div>
+          ) : (
+            <>
+              <div> You have {orderItems.length} elements in your order</div>
+              {orderItems.map((item, index) => {
+                console.log(item.count);
+
+                return (
+                  <div className="flex flex-row" key={index}>
+                    <div>{item.name}</div>
+                    <span> x {item.count}</span>
+                    <button onClick={() => removeFromCart(item)}>Remove</button>
+                  </div>
+                );
+              })}
+              <button>Pass your order</button>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
